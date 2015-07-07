bbe29e7f1261e4cde14461b1b09f1f2e11969cc2
Jacob Bieker <jacob@bieker.us>
2015-07-07 14:53:01 -0700

    Add basic import of XLConnect

---
+++ genomics_import.R
@@ -0,0 +1,28 @@
+#          Designed and Developed by Jacob Bieker (jacob@bieker.us)
+#     
+#       This script is designed convert input data to a standard format for
+#       use Brenden-Colson Center and Stand Up 2 Cancer. 
+#
+#       Assumptions:
+#       - Raw data will be passed to this file in an Excel document to be parsed and saved to 
+#       an Rdata file for use in the view script
+#
+
+##############################################################################
+#
+#                 Setup
+#
+##############################################################################
+# Check if libraries are installed, if not, install them
+if(require("XLConnect")){
+  print("XLConnect are loaded correctly")
+} else {
+  print("trying to install XLConnect")
+  install.packages("XLConnet")
+  if(require("XLConnect"){
+    print("XLConnect are installed and loaded")
+  } else {
+    stop("could not install XLConnect")
+  }
+}
+
