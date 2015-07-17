6315e1d304ac53f789ac4667ce81056f2c28244b
Jacob Bieker <jacob@bieker.us>
2015-07-16 19:43:40 -0700

    Revert "Update diagrams with LabKey WIP script"
    This reverts commit 88a374959a56bbdab5a35d54c62f16383417873b.

--- genomic_diagrams.R
+++ genomic_diagrams.R
@@ -4,7 +4,8 @@
 #       Brenden-Colson Center and Stand Up 2 Cancer. 
 #
 #       Assumptions:
-#       - Average is taken by t_alt_count/(t_alt_count+t_ref_count)
+#       - Data will be passed from another R script in a standard format to this
+#       script.
 #
 ##############################################################################
 #
@@ -12,70 +13,14 @@
 #
 ##############################################################################
 # Check if libraries are installed, if not, install them
-#if(require("VennDiagram")){
-#  print("VennDiagram are loaded correctly")
-#} else {
-#  print("trying to installVennDiagram")
-#  
-#install.packages("VennDiagram")
-#}
-
-require(Cairo)
-
-tumor_data <- labkey.data[ with(labkey.data,  grepl("T_tumor", sample)  & !is.na(labkey.data$sample) ) , ]
-metastasis_data <- labkey.data[ with(labkey.data,  grepl("M_tumor", sample)  & !is.na(labkey.data$sample) ) , ]
-
-# Get percentages
-average_t <- tumor_data$t_alt_count / (tumor_data$t_alt_count + tumor_data$t_ref_count);
-#Add average back in so we can sort later to get right names
-tumor_data <- cbind(tumor_data, average_t);
-#Sort from biggest to smallest
-average_t <- sort(average_t, decreasing = TRUE);
-
-#Switch order of variants around to match sorted averages
-tumor_data <- tumor_data[match(average_t, tumor_data$average_t),];
-
-# Getting the labels to put on the plots
-t_labels <- c();
-t_variants <- tumor_data$variant;
-
-for (t_variant in t_variants) {
-  t_parts <- strsplit(t_variant, ":");
-  t_labels <-c(t_labels, unique(rapply(t_parts, function(x) head(x, 1))))
+if(require("VennDiagram")){
+  print("VennDiagram are loaded correctly")
+} else {
+  print("trying to installVennDiagram")
+  install.packages("VennDiagram")
 }
 
-#Get percentages
-average_m <- metastasis_data$t_alt_count / (metastasis_data$t_alt_count + metastasis_data$t_ref_count);
-#Add average back in so we can sort later to get right names
-metastasis_data <- cbind(metastasis_data, average_m);
-#Sort from biggest to smallest
-average_m <- sort(average_m, decreasing = TRUE);
-
-#Switch order of variants around to match sorted averages
-metastasis_data <- metastasis_data[match(average_m, metastasis_data$average_m),];
-
-# Getting the labels to put on the plots
-m_labels <- c();
-m_variants <- metastasis_data$variant;
-
-for (m_variant in m_variants) {
-  m_parts <- strsplit(m_variant, ":");
-  m_labels <-c(m_labels, unique(rapply(m_parts, function(x) head(x, 1))))
-}
-
-##################################################
-#
-#           Graphing bar plot data
-#
-##################################################
-#   Open a Cairo device to take your plotting output:
-Cairo(file="${imgout:Primary_barplot.png}", type="png");
-#  Plot a LabKey L:
-barplot(average_t, ylab= "Percentage", names.arg=t_labels,axis.lty=3, cex.names=0.8, las=3, main="Average Test Percentage");
-dev.off();
-
-#   Open a Cairo device to take your plotting output:
-Cairo(file="${imgout:Metastasis_barplot.png}", type="png");
-#  Plot a LabKey L:
-barplot(average_m, ylab= "Percentage", names.arg=m_labels, axis.lty=3, cex.names=0.8, las=3, main="Average Test Percentage");
-dev.off();
+# Load data from the Rdata file created by the data import file
+# Assumptions: there is a data.frame called genomics that contains the data needed
+metastasis_data <- readRDS("metastasis.rds")
+tumor_data <- readRDS('tumor.rds')
--- genomics_import.R
+++ genomics_import.R
@@ -23,12 +23,10 @@ if(require("XLConnect")){
 
 workbook <- loadWorkbook("data.xlsx")
 
-labkey.data <- readWorksheet(workbook, sheet = 1, header = TRUE)
-for (variant in variants) {
-  
-}
-tumor_labkey.data <- labkey.data[ with(labkey.data,  grepl("T_tumor", Sample)  & !is.na(labkey.data$Sample) ) , ]
-metastasis_labkey.data <- labkey.data[ with(labkey.data,  grepl("M_tumor", Sample)  & !is.na(labkey.data$Sample) ) , ]
+data_file <- readWorksheet(workbook, sheet = 1, header = TRUE)
 
-saveRDS(tumor_labkey.data, file = "tumor.rds")
-saveRDS(metastasis_labkey.data, file = "metastasis.rds")
+tumor_data_file <- data_file[ with(data_file,  grepl("T_tumor", Sample)  & !is.na(data_file$Sample) ) , ]
+metastasis_data_file <- data_file[ with(data_file,  grepl("M_tumor", Sample)  & !is.na(data_file$Sample) ) , ]
+
+saveRDS(tumor_data_file, file = "tumor.rds")
+saveRDS(metastasis_data_file, file = "metastasis.rds")
