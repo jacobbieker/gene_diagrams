44431ee750a233b69194ba26c03f39f203e07310
Jacob Bieker <jacob@bieker.us>
2015-08-03 12:15:29 -0700

    Fix scaling issues on points
    Also removed unused code

--- genomic_diagrams.R
+++ genomic_diagrams.R
@@ -13,64 +13,7 @@
 ##############################################################################
 # Check if libraries are installed, if not, install them
 require(Cairo)
-
-# Taken From: https://stackoverflow.com/questions/3402371/combine-two-data-frames-by-rows-rbind-when-they-have-different-sets-of-columns
-### combines data frames (like rbind) but by matching column names
-# columns without matches in the other data frame are still combined
-# but with NA in the rows corresponding to the data frame without
-# the variable
-# A warning is issued if there is a type mismatch between columns of
-# the same name and an attempt is made to combine the columns
-combineByName <- function(A,B) {
-  a.names <- names(A)
-  b.names <- names(B)
-  all.names <- union(a.names,b.names)
-  print(paste("Number of columns:",length(all.names)))
-  a.type <- NULL
-  for (i in 1:ncol(A)) {
-    a.type[i] <- typeof(A[,i])
-  }
-  b.type <- NULL
-  for (i in 1:ncol(B)) {
-    b.type[i] <- typeof(B[,i])
-  }
-  a_b.names <- names(A)[!names(A)%in%names(B)]
-  b_a.names <- names(B)[!names(B)%in%names(A)]
-  if (length(a_b.names)>0 | length(b_a.names)>0){
-    print("Columns in data frame A but not in data frame B:")
-    print(a_b.names)
-    print("Columns in data frame B but not in data frame A:")
-    print(b_a.names)
-  } else if(a.names==b.names & a.type==b.type){
-    C <- rbind(A,B)
-    return(C)
-  }
-  C <- list()
-  for(i in 1:length(all.names)) {
-    l.a <- all.names[i]%in%a.names
-    pos.a <- match(all.names[i],a.names)
-    typ.a <- a.type[pos.a]
-    l.b <- all.names[i]%in%b.names
-    pos.b <- match(all.names[i],b.names)
-    typ.b <- b.type[pos.b]
-    if(l.a & l.b) {
-      if(typ.a==typ.b) {
-        vec <- c(A[,pos.a],B[,pos.b])
-      } else {
-        warning(c("Type mismatch in variable named: ",all.names[i],"\n"))
-        vec <- try(c(A[,pos.a],B[,pos.b]))
-      }
-    } else if (l.a) {
-      vec <- c(A[,pos.a],rep(0,nrow(B)))
-    } else {
-      vec <- c(rep(0,nrow(A)),B[,pos.b])
-    }
-    C[[i]] <- vec
-  }
-  names(C) <- all.names
-  C <- as.data.frame(C)
-  return(C)
-}
+require(plotrix)
 ##############################################################################
 #
 #                 End Setup
@@ -195,6 +138,6 @@ metastasis_scatterplot.data <- as.numeric(as.vector(metastasis_data.frame[1,]))
 #   Open a Cairo device to take your plotting output:
 Cairo(file="${imgout:Primary_Metastasis.png}", type="png");
 #  Plot:
-plot(tumor_scatterplot.data, xlim=c(0:1), ylim=c(0:1), metastasis_scatterplot.data, ylab= "Metastasis", xlab = "Primary", main="Primary vs Metastasis Mutations");
+plot(tumor_scatterplot.data, metastasis_scatterplot.data,  powscale=FALSE, scale=0.1, size=c(0.15, 1), xlim=c(0:1), ylim=c(0:1), ylab= "Metastasis", xlab = "Primary", main="Primary vs Metastasis Mutations");
 abline(0,1, lty=3);
 dev.off();
